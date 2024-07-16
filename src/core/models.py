076f079e9c6972c82d693f66739495eae0e736ee
ImperatorNeron <katebarspk@gmail.com>
2024-07-17 02:20:42 +0300

    #6 | Written TaskTrackerBaseModel class

---
+++ src/core/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import DeclarativeBase, declared_attr, Mapped, mapped_column
+
+
+class TaskTrackerBaseModel(DeclarativeBase):
+    __abstract__ = True
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    @declared_attr.directive
+    def __tablename__(self) -> str:
+        return f"{self.__name__.lower()}s"
+Configured alembic for migrations
