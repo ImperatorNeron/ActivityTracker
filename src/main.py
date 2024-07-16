fa7ebd4f39716296ac285d7bdf3fe840ef1246be
ImperatorNeron <katebarspk@gmail.com>
2024-07-17 01:11:55 +0300

    #2 | Created global router, added main/settings files

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+import uvicorn
+
+from src.core.router import router
+
+application = FastAPI(title="Task Tracker")
+
+application.include_router(router)
+
+if __name__ == '__main__':
+    uvicorn.run("main:application", reload=True)
