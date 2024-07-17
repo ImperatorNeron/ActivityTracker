89a98e7daca6b02efe436106e282c42781662ab7
ImperatorNeron <katebarspk@gmail.com>
2024-07-17 03:17:37 +0300

    #10 | Created migration

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+__all__ = (
+    "TaskTrackerBaseModel",
+    "User",
+)
+
+from src.user.models import User
+
+from src.core.models import TaskTrackerBaseModel
--- src/core/models.py
+++ src/core/models.py
@@ -1,8 +1,15 @@
+from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase, declared_attr, Mapped, mapped_column
+
+from src.core.settings import settings
 
 
 class TaskTrackerBaseModel(DeclarativeBase):
     __abstract__ = True
+
+    metadata = MetaData(
+        naming_convention=settings.database.naming_convention
+    )
 
     id: Mapped[int] = mapped_column(primary_key=True)
 
