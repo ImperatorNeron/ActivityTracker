bb5fa3e8e1e2766b91e5edc3af53d6afb2129077
ImperatorNeron <katebarspk@gmail.com>
2024-07-17 03:17:00 +0300

    #9 | Added naming convention, corrected alembic revision filename format

--- src/core/settings.py
+++ src/core/settings.py
@@ -12,6 +12,14 @@ class DatabaseSettings(BaseSettings):
     DB_NAME: str
     echo: bool = False
 
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+
     @property
     def url(self):
         return (
