ad792414954533e18485d2ef3454bd25ebc1c55c
ImperatorNeron <katebarspk@gmail.com>
2024-07-17 17:53:20 +0300

    #30 | Created auth router

--- src/core/router.py
+++ src/core/router.py
@@ -1,4 +1,7 @@
 from fastapi import APIRouter
 from src.core.settings import settings
+from src.authentication.views import router as auth_router
 
 router = APIRouter(prefix=settings.api_version_prefix)
+
+router.include_router(auth_router)
