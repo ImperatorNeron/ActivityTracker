3f260fb41e14b9e82b5ee85bdeb16299198f63e8
ImperatorNeron <katebarspk@gmail.com>
2024-07-17 16:50:34 +0300

    #28 | Updated user schema

--- src/user/schemas.py
+++ src/user/schemas.py
@@ -1,13 +1,18 @@
 from fastapi_users import schemas
+from pydantic import BaseModel, Field
 
 
-class UserRead(schemas.BaseUser[int]):
+class CustomUserBase(BaseModel):
+    nickname: str | None = Field(max_length=100)
+
+
+class UserRead(schemas.BaseUser[int], CustomUserBase):
     pass
 
 
-class UserCreate(schemas.BaseUserCreate):
+class UserCreate(schemas.BaseUserCreate, CustomUserBase):
     pass
 
 
-class UserUpdate(schemas.BaseUserUpdate):
+class UserUpdate(schemas.BaseUserUpdate, CustomUserBase):
     pass
