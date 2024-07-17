ca22534fba37f21d44e56c01d6fa4de8f3af17cc
ImperatorNeron <katebarspk@gmail.com>
2024-07-17 15:29:42 +0300

    #25 | Created user manager

---
+++ src/authentication/user_manager.py
@@ -0,0 +1,46 @@
+import logging
+from typing import Optional, TYPE_CHECKING
+
+from fastapi_users import BaseUserManager, IntegerIDMixin
+
+from src.user.models import User
+from src.core.settings import settings
+
+if TYPE_CHECKING:
+    from fastapi import Request
+
+log = logging.getLogger(__name__)
+
+
+class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
+    reset_password_token_secret = settings.access_token.reset_password_token_secret
+    verification_token_secret = settings.access_token.verification_token_secret
+
+    async def on_after_register(
+        self,
+        user: User,
+        request: Request | None = None,
+    ):
+        log.warning(f"User %r has registered.", user.id)
+
+    async def on_after_forgot_password(
+        self,
+        user: User,
+        token: str,
+        request: Request | None = None,
+    ):
+        log.warning(
+            f"User %r has forgot their password. Reset token: %r", user.id, token
+        )
+
+    async def on_after_request_verify(
+        self,
+        user: User,
+        token: str,
+        request: Request | None = None,
+    ):
+        log.warning(
+            f"Verification requested for user %r. Verification token: %r",
+            user.id,
+            token,
+        )
