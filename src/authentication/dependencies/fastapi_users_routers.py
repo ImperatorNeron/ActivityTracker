fc4d4872b83576aace82cbdc38e7baa7cac13149
ImperatorNeron <katebarspk@gmail.com>
2024-07-17 18:02:55 +0300

    #31 | Included auth router

--- src/authentication/dependencies/fastapi_users_routers.py
+++ src/authentication/dependencies/fastapi_users_routers.py
@@ -4,7 +4,7 @@ from src.user.models import User
 from src.authentication.dependencies.user_manager import get_user_manager
 from src.authentication.dependencies.backend import authentication_backend
 
-fastapi_users = FastAPIUsers[User, int](
+fastapi_users_routers = FastAPIUsers[User, int](
     get_user_manager,
     [authentication_backend],
 )
--- src/authentication/views.py
+++ src/authentication/views.py
@@ -1,6 +1,13 @@
 from fastapi import APIRouter
 
+from src.authentication.dependencies.backend import authentication_backend
+from src.authentication.dependencies.fastapi_users_routers import fastapi_users_routers
+
 router = APIRouter(
     prefix="/auth",
     tags=["Auth"]
+)
+
+router.include_router(
+    router=fastapi_users_routers.get_auth_router(authentication_backend),
 )
