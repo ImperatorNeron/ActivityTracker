d3f066250aa214d31f8a503e0830a5718291261e
ImperatorNeron <katebarspk@gmail.com>
2024-07-17 13:01:44 +0300

    #13 | Created DatabaseHelper class with initialization

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,9 @@
 __all__ = (
     "TaskTrackerBaseModel",
     "User",
+    "database_helper",
 )
 
+from src.core.database_helper import database_helper
 from src.user.models import User
-
 from src.core.models import TaskTrackerBaseModel
---
+++ src/core/database_helper.py
@@ -0,0 +1,50 @@
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    AsyncEngine,
+    async_sessionmaker,
+    AsyncSession,
+)
+
+from src.core.settings import settings
+
+
+class DatabaseHelper:
+    def __init__(
+        self,
+        url: str,
+        echo: bool = False,
+        echo_pool: bool = False,
+        pool_size: int = 5,
+        max_overflow: int = 10,
+    ) -> None:
+        self.engine: AsyncEngine = create_async_engine(
+            url=url,
+            echo=echo,
+            echo_pool=echo_pool,
+            pool_size=pool_size,
+            max_overflow=max_overflow,
+        )
+        self.session_factory: async_sessionmaker[AsyncSession] = async_sessionmaker(
+            bind=self.engine,
+            autoflush=False,
+            autocommit=False,
+            expire_on_commit=False,
+        )
+
+    async def dispose(self) -> None:
+        await self.engine.dispose()
+
+    async def session_getter(self) -> AsyncGenerator[AsyncSession, None]:
+        async with self.session_factory() as session:
+            yield session
+
+
+database_helper = DatabaseHelper(
+    url=str(settings.database.url),
+    echo=settings.database.echo,
+    echo_pool=settings.database.echo_pool,
+    pool_size=settings.database.pool_size,
+    max_overflow=settings.database.max_overflow,
+)
