f221e7c09ae1593b01617e4a985eba57f34e638c
ImperatorNeron <katebarspk@gmail.com>
2024-07-17 14:01:31 +0300

    #20 | Refactored main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,24 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 import uvicorn
+from fastapi.responses import ORJSONResponse
 
+from src.core.database_helper import database_helper
 from src.core.router import router
 
-application = FastAPI(title="Task Tracker")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    yield
+    # shutdown
+    await database_helper.dispose()
+
+
+application = FastAPI(
+    title="Task Tracker", default_response_class=ORJSONResponse, lifespan=lifespan
+)
 application.include_router(router)
 
 if __name__ == "__main__":
