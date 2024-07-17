b284b64ae49e8f36fc6939d03909942af8c135fa
ImperatorNeron <katebarspk@gmail.com>
2024-07-17 17:47:12 +0300

    #29 | Created fastapi_users_routers

---
+++ src/authentication/dependencies/fastapi_users_routers.py
@@ -0,0 +1,10 @@
+from fastapi_users import FastAPIUsers
+
+from src.user.models import User
+from src.authentication.dependencies.user_manager import get_user_manager
+from src.authentication.dependencies.backend import authentication_backend
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [authentication_backend],
+)
