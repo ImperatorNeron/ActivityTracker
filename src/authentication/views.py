7ce9a02138b8c2a36675dd5dc336c95fa0c13a58
ImperatorNeron <katebarspk@gmail.com>
2024-07-18 13:45:21 +0300

    #33 | Added router to register

--- src/authentication/views.py
+++ src/authentication/views.py
@@ -2,12 +2,13 @@ from fastapi import APIRouter
 
 from src.authentication.dependencies.backend import authentication_backend
 from src.authentication.dependencies.fastapi_users_routers import fastapi_users_routers
+from src.user.schemas import UserRead, UserCreate
 
-router = APIRouter(
-    prefix="/auth",
-    tags=["Auth"]
-)
+router = APIRouter(prefix="/auth", tags=["Auth"])
 
 router.include_router(
     router=fastapi_users_routers.get_auth_router(authentication_backend),
+)
+router.include_router(
+    router=fastapi_users_routers.get_register_router(UserRead, UserCreate),
 )
