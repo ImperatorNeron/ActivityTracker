609477f80f9b5e92a9f5e62897862e9d04468919
ImperatorNeron <katebarspk@gmail.com>
2024-07-19 16:52:13 +0300

    #56 | Migrated Folder model

--- src/__init__.py
+++ src/__init__.py
@@ -11,3 +11,4 @@ from src.core.database_helper import database_helper
 from src.core.models import TaskTrackerBaseModel
 from src.user.models import User
 from src.uom.models import UOM
+from src.folder.models import Folder
