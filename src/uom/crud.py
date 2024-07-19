5e9d94341759416a4e68e4b7679de30a2daaa032
ImperatorNeron <katebarspk@gmail.com>
2024-07-19 23:18:58 +0300

    #70 | Created partial_update_uom and get_uom

--- src/uom/crud.py
+++ src/uom/crud.py
@@ -1,9 +1,9 @@
-from typing import TYPE_CHECKING, List
+from typing import TYPE_CHECKING, List, Optional
 
 from sqlalchemy import select
 
 from src.uom.models import UOM
-from src.uom.schemas import UOMCreate
+from src.uom.schemas import UOMCreate, UOMPartialUpdate
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
@@ -22,3 +22,19 @@ async def add_uom(uom: UOMCreate, session: "AsyncSession") -> UOM:
     session.add(new_uom)
     await session.commit()
     return new_uom
+
+
+async def partial_update_uom(
+    uom_in: UOMPartialUpdate,
+    uom_now_id: int,
+    session: "AsyncSession",
+) -> UOM:
+    uom = await get_uom(uom_now_id, session)
+    for key, value in uom_in.model_dump(exclude_unset=True).items():
+        setattr(uom, key, value)
+    await session.commit()
+    return uom
+
+
+async def get_uom(uom_id: int, session: "AsyncSession") -> Optional[UOM]:
+    return await session.get(UOM, uom_id)
