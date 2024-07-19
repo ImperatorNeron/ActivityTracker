267ee117aa059dc1f53cc8e162d1b0a2c4b399a9
ImperatorNeron <katebarspk@gmail.com>
2024-07-19 16:51:47 +0300

    #55 | Added relation to User model

--- src/user/models.py
+++ src/user/models.py
@@ -1,17 +1,19 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, List
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.core.mixins import IdIntPkMixin
 from src.core.models import TaskTrackerBaseModel
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
+    from src.folder.models import Folder
 
 
 class User(IdIntPkMixin, TaskTrackerBaseModel, SQLAlchemyBaseUserTable[int]):
     nickname: Mapped[str] = mapped_column("nickname", String(100), default="Newbie")
+    folders: Mapped[List["Folder"]] = relationship("Folder", back_populates="user")
 
     @classmethod
     def get_db(cls, session: "AsyncSession"):
