912952ffcccaebb8427433cecd5584f0a858b83f
ImperatorNeron <katebarspk@gmail.com>
2024-07-19 12:45:55 +0300

    #42 | Added get_verify_router

--- src/authentication/views.py
+++ src/authentication/views.py
@@ -9,6 +9,11 @@ router = APIRouter(prefix="/auth", tags=["Auth"])
 router.include_router(
     router=fastapi_users_routers.get_auth_router(authentication_backend),
 )
+
 router.include_router(
     router=fastapi_users_routers.get_register_router(UserRead, UserCreate),
+)
+
+router.include_router(
+    router=fastapi_users_routers.get_verify_router(UserRead)
 )
