ee2b5f8c70ffd825117afd3ba2ff1cdcc47be7f5
ImperatorNeron <katebarspk@gmail.com>
2024-07-19 16:49:09 +0300

    #53 | Created UserRelationMixin for adding relation in certain model

--- src/core/mixins.py
+++ src/core/mixins.py
@@ -1,6 +1,11 @@
-from sqlalchemy import Integer
-from sqlalchemy.orm import Mapped
+from typing import TYPE_CHECKING, Optional
+
+from sqlalchemy import Integer, ForeignKey
+from sqlalchemy.orm import Mapped, declared_attr, relationship
 from sqlalchemy.orm import mapped_column
+
+if TYPE_CHECKING:
+    from src.user.models import User
 
 
 class IdIntPkMixin:
@@ -10,3 +15,24 @@ class IdIntPkMixin:
         index=True,
         autoincrement=True,
     )
+
+
+class UserRelationMixin:
+    _nullable: Optional[bool] = False
+    _back_populates: Optional[str] = None
+    _primary_key: Optional[bool] = False
+
+    @declared_attr
+    def user_id(self) -> Mapped[int]:
+        return mapped_column(
+            ForeignKey("users.id"),
+            nullable=self._nullable,
+            primary_key=self._primary_key
+        )
+
+    @declared_attr
+    def user(self) -> Mapped["User"]:
+        return relationship(
+            "User",
+            back_populates=self._back_populates,
+        )
