349d995240e0b8d8b0f43e673cf1bbf70a40e19d
ImperatorNeron <katebarspk@gmail.com>
2024-07-19 15:22:02 +0300

    #50 | Created get_uom_list viewpoint

--- src/uom/views.py
+++ src/uom/views.py
@@ -1,6 +1,23 @@
-from fastapi import APIRouter
+from typing import Annotated, TYPE_CHECKING, List
+from fastapi import APIRouter, Depends
+from src.core.database_helper import database_helper
+from src.uom import crud
+from src.uom.schemas import UOMRead
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
 
 router = APIRouter(
     prefix="/uom",
     tags=["Unit of measurement"],
 )
+
+
+@router.get("/", response_model=List[UOMRead])
+async def get_uom_list(
+    session: Annotated[
+        "AsyncSession",
+        Depends(database_helper.session_getter),
+    ],
+):
+    return await crud.uom_list(session)
