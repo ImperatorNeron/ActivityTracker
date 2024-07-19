9c354422662dbb01cb244994677a3e478b573cdd
ImperatorNeron <katebarspk@gmail.com>
2024-07-19 14:41:10 +0300

    #47 | Created and add uom router

--- src/core/router.py
+++ src/core/router.py
@@ -4,13 +4,14 @@ from fastapi.security import HTTPBearer
 from src.core.settings import settings
 from src.authentication.views import router as auth_router
 from src.user.views import router as users_router
+from src.uom.views import router as uom_router
 
 http_bearer = HTTPBearer(auto_error=False)
 
 router = APIRouter(
-    prefix=settings.api_version_prefix,
-    dependencies=[Depends(http_bearer)]
+    prefix=settings.api_version_prefix, dependencies=[Depends(http_bearer)]
 )
 
 router.include_router(auth_router)
 router.include_router(users_router)
+router.include_router(uom_router)
