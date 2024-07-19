c730338a4c9bfa9075ab43f1b67e73153ab65576
ImperatorNeron <katebarspk@gmail.com>
2024-07-19 14:32:31 +0300

    #46 | Migrated UOM model to database

--- src/__init__.py
+++ src/__init__.py
@@ -3,9 +3,11 @@ __all__ = (
     "TaskTrackerBaseModel",
     "User",
     "AccessToken",
+    "UOM"
 )
 
 from src.authentication.models import AccessToken
 from src.core.database_helper import database_helper
 from src.core.models import TaskTrackerBaseModel
 from src.user.models import User
+from src.uom.models import UOM
