e0761b98420c86e55bd025971216da4748e7c793
ImperatorNeron <katebarspk@gmail.com>
2024-07-19 21:51:42 +0300

    #61 | Added crud function for creating folders

--- src/folder/crud.py
+++ src/folder/crud.py
@@ -2,6 +2,7 @@ from typing import List, TYPE_CHECKING
 
 from sqlalchemy import select
 from src.folder.models import Folder
+from src.folder.schemas import FolderCreate
 
 if TYPE_CHECKING:
     from sqlalchemy import Result
@@ -13,3 +14,14 @@ async def get_folders(user_id: int, session: "AsyncSession") -> List[Folder]:
         select(Folder).where(Folder.user_id == user_id).order_by(Folder.title),
     )
     return list(result.scalars().all())
+
+
+async def create_folder(
+    folder_in: FolderCreate,
+    user_id: int,
+    session: "AsyncSession",
+) -> Folder:
+    folder = Folder(**folder_in.model_dump(), user_id=user_id)
+    session.add(folder)
+    await session.commit()
+    return folder
