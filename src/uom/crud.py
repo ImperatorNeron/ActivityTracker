23c4df6120bb41ec8232fb54047b9d6c320741d4
ImperatorNeron <katebarspk@gmail.com>
2024-07-19 15:31:29 +0300

    #51 | Created crud operation and viewpoint to add uom

--- src/uom/crud.py
+++ src/uom/crud.py
@@ -3,6 +3,7 @@ from typing import TYPE_CHECKING, List
 from sqlalchemy import select
 
 from src.uom.models import UOM
+from src.uom.schemas import UOMCreate
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
@@ -14,3 +15,10 @@ async def uom_list(session: "AsyncSession") -> List[UOM]:
         select(UOM).order_by(UOM.id),
     )
     return list(result.scalars().all())
+
+
+async def add_uom(uom: UOMCreate, session: "AsyncSession") -> UOM:
+    new_uom = UOM(**uom.model_dump())
+    session.add(new_uom)
+    await session.commit()
+    return new_uom
--- src/uom/views.py
+++ src/uom/views.py
@@ -2,7 +2,7 @@ from typing import Annotated, TYPE_CHECKING, List
 from fastapi import APIRouter, Depends
 from src.core.database_helper import database_helper
 from src.uom import crud
-from src.uom.schemas import UOMRead
+from src.uom.schemas import UOMRead, UOMCreate
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
@@ -21,3 +21,14 @@ async def get_uom_list(
     ],
 ):
     return await crud.uom_list(session)
+
+
+@router.post("/", response_model=UOMRead)
+async def add_uom(
+    uom: UOMCreate,
+    session: Annotated[
+        "AsyncSession",
+        Depends(database_helper.session_getter),
+    ],
+):
+    return await crud.add_uom(uom, session)
