a042784998041304043adc8c93d1cb501dc02ed8
ImperatorNeron <katebarspk@gmail.com>
2024-07-24 02:13:24 +0300

    #75 | Created router and added to main router

--- src/core/router.py
+++ src/core/router.py
@@ -6,6 +6,7 @@ from src.authentication.views import router as auth_router
 from src.user.views import router as users_router
 from src.uom.views import router as uom_router
 from src.folder.views import router as folder_router
+from src.task.views import router as task_router
 
 http_bearer = HTTPBearer(auto_error=False)
 
@@ -17,3 +18,4 @@ router.include_router(auth_router)
 router.include_router(users_router)
 router.include_router(uom_router)
 router.include_router(folder_router)
+router.include_router(task_router)
