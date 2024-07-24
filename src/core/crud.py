bd8aaa812782b8a7ecb0235615727eea50ddefad
ImperatorNeron <katebarspk@gmail.com>
2024-07-24 20:41:51 +0300

    #83 | Created basic crud function for create item

--- src/core/crud.py
+++ src/core/crud.py
@@ -1,4 +1,4 @@
-from typing import List, TYPE_CHECKING
+from typing import List, TYPE_CHECKING, Any
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
@@ -8,8 +8,22 @@ if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
 
 
-async def get_items_by_user_id(entity, user_id: int, session: AsyncSession) -> List:
+async def get_items_by_user_id(
+    entity: Any, user_id: int, session: AsyncSession
+) -> List:
     result: "Result" = await session.execute(
         select(entity).where(entity.user_id == user_id).order_by(entity.id)
     )
     return list(result.scalars().all())
+
+
+async def create_item_by_user_id(
+    entity: Any,
+    item_in: Any,
+    user_id: int,
+    session: AsyncSession,
+) -> Any:
+    item = entity(**item_in.model_dump(), user_id=user_id)
+    session.add(item)
+    await session.commit()
+    return item
