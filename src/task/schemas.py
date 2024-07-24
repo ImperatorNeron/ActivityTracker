3f2ffea4fa0b23bc00b62e0fd605146874248eb5
ImperatorNeron <katebarspk@gmail.com>
2024-07-24 20:26:38 +0300

    #81 | Created schemas for create and update tasks

--- src/task/schemas.py
+++ src/task/schemas.py
@@ -11,9 +11,23 @@ class TaskBase(BaseModel):
     goal_value: float = Field(ge=0)
     start_date: Optional[datetime] = None
     finish_date: Optional[datetime] = None
-    folder_id: Optional[datetime] = Field(ge=0, default=None)
+    folder_id: Optional[int] = Field(ge=0, default=None)
 
 
 class ReadTask(TaskBase):
     id: int = Field(ge=0)
     creation_date: datetime
+
+
+class CreateTask(TaskBase):
+    pass
+
+
+class UpdateTask(TaskBase):
+    title: Optional[str] = None
+    description: Optional[str] = None
+    start_value: Optional[float] = None
+    goal_value: Optional[float] = None
+    start_date: Optional[datetime] = None
+    finish_date: Optional[datetime] = None
+    folder_id: Optional[datetime] = None
