e9c3351542003cc347e86c8971bedd65eaf8759d
ImperatorNeron <katebarspk@gmail.com>
2024-07-24 21:34:43 +0300

    #88 | Added viewpoints to get and delete task by id

--- src/task/views.py
+++ src/task/views.py
@@ -3,10 +3,15 @@ from typing import Annotated, List
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src import User
+from src import User, Task
 from src.authentication.dependencies.fastapi_users_routers import current_active_user
+from src.core.crud import (
+    get_user_item_by_id,
+    delete_item_by_user_id,
+    create_item_by_user_id,
+    get_items_by_user_id,
+)
 from src.core.database_helper import database_helper
-from src.task import crud
 from src.task.schemas import ReadTask, CreateTask
 
 router = APIRouter(
@@ -26,7 +31,7 @@ async def get_tasks(
         Depends(current_active_user),
     ],
 ):
-    return await crud.get_tasks(current_user.id, session)
+    return await get_items_by_user_id(Task, current_user.id, session)
 
 
 @router.post("/create", response_model=ReadTask)
@@ -41,4 +46,34 @@ async def create_task(
         Depends(current_active_user),
     ],
 ):
-    return await crud.create_task(task_in, current_user.id, session)
+    return await create_item_by_user_id(Task, task_in, current_user.id, session)
+
+
+@router.get("/{task_id}", response_model=ReadTask)
+async def get_task_by_id(
+    task_id: int,
+    session: Annotated[
+        AsyncSession,
+        Depends(database_helper.session_getter),
+    ],
+    current_user: Annotated[  # noqa
+        User,
+        Depends(current_active_user),
+    ],
+):
+    return await get_user_item_by_id(Task, task_id, current_user.id, session)
+
+
+@router.delete("/delete/{task_id}")
+async def delete_task(
+    task_id: int,
+    session: Annotated[
+        AsyncSession,
+        Depends(database_helper.session_getter),
+    ],
+    current_user: Annotated[  # noqa
+        User,
+        Depends(current_active_user),
+    ],
+):
+    return await delete_item_by_user_id(Task, task_id, current_user.id, session)
