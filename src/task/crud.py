34b6d255c2a87565604e123ad5cc82165d4cdf95
ImperatorNeron <katebarspk@gmail.com>
2024-07-24 20:29:02 +0300

    #82 | Created crud and viewpoint to add task

--- src/task/crud.py
+++ src/task/crud.py
@@ -1,14 +1,22 @@
-from typing import List, TYPE_CHECKING
+from typing import List
 
-from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src import Task
 from src.core import crud
-
-if TYPE_CHECKING:
-    from sqlalchemy import Result
+from src.task.schemas import CreateTask
 
 
 async def get_tasks(user_id: int, session: AsyncSession) -> List["Task"]:
     return await crud.get_items_by_user_id(Task, user_id, session)
+
+
+async def create_task(
+    task_in: CreateTask,
+    user_id: int,
+    session: AsyncSession,
+) -> Task:
+    task = Task(**task_in.model_dump(), user_id=user_id)
+    session.add(task)
+    await session.commit()
+    return task
--- src/task/views.py
+++ src/task/views.py
@@ -7,7 +7,7 @@ from src import User
 from src.authentication.dependencies.fastapi_users_routers import current_active_user
 from src.core.database_helper import database_helper
 from src.task import crud
-from src.task.schemas import ReadTask
+from src.task.schemas import ReadTask, CreateTask
 
 router = APIRouter(
     prefix="/tasks",
@@ -27,3 +27,18 @@ async def get_tasks(
     ],
 ):
     return await crud.get_tasks(current_user.id, session)
+
+
+@router.post("/create", response_model=ReadTask)
+async def create_task(
+    task_in: CreateTask,
+    session: Annotated[
+        AsyncSession,
+        Depends(database_helper.session_getter),
+    ],
+    current_user: Annotated[  # noqa
+        User,
+        Depends(current_active_user),
+    ],
+):
+    return await crud.create_task(task_in, current_user.id, session)
