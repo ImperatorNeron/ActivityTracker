2a56e93c73ac6ebf7e3bdf1b6d3ee12cc5f123c8
ImperatorNeron <katebarspk@gmail.com>
2024-07-24 21:32:23 +0300

    #85 | Added get_user_item_by_id and delete_item_by_user_id functions in base crud

--- src/core/crud.py
+++ src/core/crud.py
@@ -1,4 +1,5 @@
-from typing import List, TYPE_CHECKING, Any
+from typing import List, TYPE_CHECKING, Any, TypeVar
+from fastapi import status, HTTPException
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
@@ -27,3 +28,32 @@ async def create_item_by_user_id(
     session.add(item)
     await session.commit()
     return item
+
+
+async def get_user_item_by_id(
+    entity: Any,
+    item_id: int,
+    user_id: int,
+    session: AsyncSession,
+) -> Any:
+    item = await session.get(
+        entity,
+        item_id,
+    )
+    if item.user_id == user_id:
+        return item
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"There is no such object!",
+    )
+
+
+async def delete_item_by_user_id(
+    entity: Any,
+    item_id: int,
+    user_id: int,
+    session: AsyncSession,
+) -> dict[str, str]:
+    await session.delete(await get_user_item_by_id(entity, item_id, user_id, session))
+    await session.commit()
+    return {"status_code": status.HTTP_204_NO_CONTENT}
