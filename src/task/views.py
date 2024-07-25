1f52ca0b2cc7e0a0f5ad9999f8e335618f6d57bb
ImperatorNeron <katebarspk@gmail.com>
2024-07-25 18:52:57 +0300

    #98 | Rewrote creation viewpoint

--- src/task/views.py
+++ src/task/views.py
@@ -8,11 +8,11 @@ from src.authentication.dependencies.fastapi_users_routers import current_active
 from src.core.crud import (
     get_user_item_by_id,
     delete_item_by_user_id,
-    create_item_by_user_id,
     get_items_by_user_id,
     update_user_item_by_id,
 )
 from src.core.database_helper import database_helper
+from src.task import crud
 from src.task.schemas import ReadTask, CreateTask, UpdateTask
 
 router = APIRouter(
@@ -35,7 +35,7 @@ async def get_tasks(
     return await get_items_by_user_id(Task, current_user.id, session)
 
 
-@router.post("/create", response_model=ReadTask)
+@router.post("/", response_model=ReadTask)
 async def create_task(
     task_in: CreateTask,
     session: Annotated[
@@ -47,7 +47,7 @@ async def create_task(
         Depends(current_active_user),
     ],
 ):
-    return await create_item_by_user_id(Task, task_in, current_user.id, session)
+    return await crud.create_task(task_in, current_user.id, session)
 
 
 @router.get("/{task_id}", response_model=ReadTask)
