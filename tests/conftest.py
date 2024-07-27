a707c1f0d7eccfe5a3c0399070b78cc1be2ec1ab
ImperatorNeron <katebarspk@gmail.com>
2024-07-28 01:54:47 +0300

    #101 | Created conftest.py with functions of getting test client for requests and for creating bearer token

---
+++ tests/conftest.py
@@ -0,0 +1,28 @@
+import pytest
+from starlette.testclient import TestClient
+
+from src.main import application
+
+
+@pytest.fixture
+def client():
+    with TestClient(app=application) as client:
+        yield client
+
+
+@pytest.fixture
+def auth_headers_for_user(client):
+    login_data = {
+        "username": "vlad@example.com",
+        "password": "1",
+    }
+    headers = {
+        "accept": "application/json",
+        "Content-Type": "application/x-www-form-urlencoded",
+    }
+    response = client.post(
+        url="/api/v1/auth/login", data=login_data, headers=headers
+    )
+    assert response.status_code == 200
+    token = response.json()["access_token"]
+    return {"Authorization": f"Bearer {token}"}
