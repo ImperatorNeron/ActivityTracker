85597cf9b79d557de32ea63ce8a29a76c8bbd954
ImperatorNeron <katebarspk@gmail.com>
2024-08-04 18:07:38 +0300

    #117 | Added new test for patch (infiltrating to another user`s folder)

--- tests/test_task/test_patch_in_wrong_folder.py
+++ tests/test_task/test_patch_in_wrong_folder.py
@@ -22,7 +22,7 @@ def test_patch_task_to_nonexistent_folder(client, auth_headers_for_user):
     headers = {"accept": "application/json", **auth_headers_for_user}
     response = client.post("/api/v1/tasks/", json=get_task_test_data(), headers=headers)
     assert response.status_code == 200
-    task_id = response.json()['id']
+    task_id = response.json()["id"]
     response = client.patch(
         f"/api/v1/tasks/{task_id}",
         json={"folder_id": "10000"},
@@ -32,3 +32,39 @@ def test_patch_task_to_nonexistent_folder(client, auth_headers_for_user):
     response = client.delete(f"/api/v1/tasks/delete/{task_id}", headers=headers)
     assert response.status_code == 200
 
+
+def test_patch_task_to_another_user_folder(
+    client,
+    auth_headers_for_user,
+    auth_headers_for_user_2,
+):
+    headers = {"accept": "application/json", **auth_headers_for_user}
+    response_1 = client.post(
+        "/api/v1/tasks/", json=get_task_test_data(), headers=headers
+    )
+    assert response_1.status_code == 200
+
+    headers_2 = {"accept": "application/json", **auth_headers_for_user_2}
+    response_2 = client.post(
+        "/api/v1/folders/", json=get_folder_test_data(), headers=headers_2
+    )
+    assert response_2.status_code == 200
+
+    task_id = response_1.json()["id"]
+    another_user_folder_id = response_2.json()["id"]
+
+    response_1 = client.patch(
+        f"/api/v1/tasks/{task_id}",
+        json={"folder_id": another_user_folder_id},
+        headers={"Content-Type": "application/json", **headers},
+    )
+    assert response_1.status_code == 404
+
+    response_1 = client.delete(f"/api/v1/tasks/delete/{task_id}", headers=headers)
+    assert response_1.status_code == 200
+
+    response_2 = client.delete(
+        f"/api/v1/folders/delete/{another_user_folder_id}",
+        headers=headers_2,
+    )
+    assert response_2.status_code == 200
