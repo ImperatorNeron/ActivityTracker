ce421c78bbdc9d9e57870d30f1ba94028ece7803
ImperatorNeron <katebarspk@gmail.com>
2024-08-05 18:10:23 +0300

    #125 | Created views file and endpoint to getting specific task`s activities list

---
+++ src/activity/views.py
@@ -0,0 +1,29 @@
+from typing import List, Annotated
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from src import User
+from src.activity import crud
+from src.activity.schemas import ActivityRead
+from src.authentication.dependencies.fastapi_users_routers import current_active_user
+from src.core.database_helper import database_helper
+
+
+router = APIRouter(
+    prefix="/activity",
+    tags=["Activities"],
+)
+
+
+@router.get("/{task_id}", response_model=List[ActivityRead])
+async def get_user_task_activity(
+    task_id: int,
+    current_user: Annotated[
+        User,
+        Depends(current_active_user),
+    ],
+    session: Annotated[
+        AsyncSession,
+        Depends(database_helper.session_getter),
+    ],
+):
+    return await crud.get_user_task_activity(task_id, current_user.id, session)
