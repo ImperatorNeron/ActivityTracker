b5e125e5a2f2bd42c4053192471a547b4a4b79ed
ImperatorNeron <katebarspk@gmail.com>
2024-08-05 18:25:32 +0300

    #128 | Added new endpoint to add activities

--- src/activity/views.py
+++ src/activity/views.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from src import User
 from src.activity import crud
-from src.activity.schemas import ActivityRead
+from src.activity.schemas import ActivityRead, ActivityCreate
 from src.authentication.dependencies.fastapi_users_routers import current_active_user
 from src.core.database_helper import database_helper
 
@@ -27,3 +27,19 @@ async def get_user_task_activity(
     ],
 ):
     return await crud.get_user_task_activity(task_id, current_user.id, session)
+
+
+@router.post("/{task_id}", response_model=ActivityRead)
+async def add_activity(
+    activity_in: ActivityCreate,
+    current_user: Annotated[
+        User,
+        Depends(current_active_user),
+    ],
+    session: Annotated[
+        AsyncSession,
+        Depends(database_helper.session_getter),
+    ],
+):
+    return await crud.add_activity(activity_in, current_user.id, session)
+
